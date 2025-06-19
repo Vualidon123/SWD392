05a2c58bedb1a502c78dc263749398591234c435
Duy Tran Lac <lacduy19@gmail.com>
2025-06-19 20:40:51 +0700

    Add School CRUD

---
+++ SPTS_Write/SPTS_Writer/Controllers/SchoolController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SPTS_Writer.Models;
+using SPTS_Writer.Service;
+
+namespace SPTS_Writer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        private readonly SchoolService _schoolService;
+
+        public SchoolController(SchoolService schoolService)
+        {
+            _schoolService = schoolService;
+        }
+        [HttpGet]
+        public IActionResult GetTest()
+        {
+            var test = _schoolService.GetAllTestsAsync().Result;
+            return Ok(test);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTestById(string id)
+        {
+            var test = await _schoolService.GetTestByIdAsync(id);
+            if (test == null)
+            {
+                return NotFound();
+            }
+            return Ok(test);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTest(School test)
+        {
+            if (test == null)
+            {
+                return BadRequest("Test cannot be null");
+            }
+            await _schoolService.AddTestAsync(test);
+            return CreatedAtAction(nameof(GetTestById), new { id = test.Id }, test);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTest(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest("Id cannot be null or empty");
+            }
+            await _schoolService.DeleteTestAsync(id);
+            return NoContent();
+        }
+    }
+}
+
---
+++ SPTS_Write/SPTS_Writer/Service/SchoolService.cs
@@ -0,0 +1,41 @@
+﻿using SPTS_Writer.Data;
+using SPTS_Writer.Models;
+
+namespace SPTS_Writer.Service
+{
+    public class SchoolService
+    {
+        private readonly IRepository<School> _schoolRepository;
+        public SchoolService(IRepository<School> schoolRepository)
+        {
+            _schoolRepository = schoolRepository;
+        }
+        public async Task<School?> GetTestByIdAsync(string id)
+        {
+            return await _schoolRepository.GetByIdAsync(id);
+        }
+        public async Task<IEnumerable<School>> GetAllTestsAsync()
+        {
+            return await _schoolRepository.GetAllAsync();
+        }
+
+        public async Task AddTestAsync(School test)
+        {
+            await _schoolRepository.AddAsync(test);
+            await _schoolRepository.SaveChangesAsync();
+        }
+
+        public async Task UpdateTestAsync(School test)
+        {
+            await _schoolRepository.UpdateAsync(test.Id.ToString(), test);
+            await _schoolRepository.SaveChangesAsync();
+        }
+        public async Task DeleteTestAsync(string id)
+        {
+            await _schoolRepository.DeleteAsync(id);
+            await _schoolRepository.SaveChangesAsync();
+        }
+
+
+    }
+}
