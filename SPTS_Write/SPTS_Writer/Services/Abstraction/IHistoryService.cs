3a797d489cb6ad2ecd80bf9ff3e58697380ed063
Sakamoto2004 <phbtoan9185@gmail.com>
2025-06-24 11:14:49 +0700

    Added interface for History and question service

---
+++ SPTS_Write/SPTS_Writer/Services/Abstraction/IHistoryService.cs
@@ -0,0 +1,11 @@
+using SPTS_Writer.Entities;
+
+namespace SPTS_Writer.Services.Abstraction;
+public interface IHistoryService
+{
+    public Task<History?> GetHistoryByIdAsync(string id);
+    public Task<IEnumerable<History>> GetAllHistorysAsync();
+    public Task AddHistoryAsync(History test);
+    public Task UpdateHistoryAsync(History test);
+    public Task<History> RecordTakenTestAsync(Test test, User who, List<Answer> answers);
+}
---
+++ SPTS_Write/SPTS_Writer/Services/Abstraction/IQuestionService.cs
@@ -0,0 +1,11 @@
+using SPTS_Writer.Entities;
+
+namespace SPTS_Writer.Services.Abstraction;
+public interface IQuestionService
+{
+    public Task<Question?> GetQuestionByIdAsync(string id);
+    public Task<IEnumerable<Question>> GetAllQuestionsAsync();
+    public Task AddQuestionAsync(Question test);
+    public Task UpdateQuestionAsync(Question test);
+}
+
