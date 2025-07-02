aaa35781619b6e0b7545032feb6223c1ae09c579
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2025-07-02 17:07:12 +0700

    Remove seed data and added test collection

--- SPTS_Read/SPTS_Reader/Data/MongoDbContext.cs
+++ SPTS_Read/SPTS_Reader/Data/MongoDbContext.cs
@@ -15,51 +15,15 @@ public class MongoDbContext
     }
 
     public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+    public IMongoCollection<Test> Tests => _database.GetCollection<Test>("Tests");
     public IMongoCollection<Test> Chats => _database.GetCollection<Test>("Tests");
     public IMongoCollection<History> Histories => _database.GetCollection<History>("Histories");
     public IMongoCollection<School> Schools => _database.GetCollection<School>("Schools");
 
     public IMongoCollection<T> GetCollection<T>()
     {
-        if (typeof(T) == typeof(User))
-            return (IMongoCollection<T>)Users;
-
+        if (typeof(T) == typeof(Test))
+            return (IMongoCollection<T>)Tests;
         throw new ArgumentException("Collection not found for the given type");
-    }
-
-    // NO, NOT HERE
-    public void SeedData()
-    {
-        // Seed Emp data
-        if (!Users.Find(_ => true).Any())
-        {
-            Users.InsertMany(new[]
-                {
-                    new User
-                    {
-                        Id= Guid.NewGuid(),
-                        Name = "John Doe",
-                        Email = "john@example.com",
-                        PhoneNumber = "123-456-7890",
-                        Password = "hashed_password_1" // In production, ensure passwords are properly hashed
-                    },
-                    new User
-                    {
-                        Id= Guid.NewGuid(),
-                        Name = "Jane Smith",
-                        Email = "jane@example.com",
-                        PhoneNumber = "123-456-7891",
-                        Password = "hashed_password_2"
-                    },
-                    new User
-                    {
-                        Id= Guid.NewGuid(),
-                        Name = "Bob Wilson",
-                        Email = "bob@example.com",
-                        PhoneNumber = "123-456-7892",
-                        Password = "hashed_password_3"
-                    }
-            });
-        }
     }
 }
